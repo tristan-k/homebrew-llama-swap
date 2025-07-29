396753bf854bcb2a668a29045c05eac45c3e5ea5
Ian Sebastian Mathew <dev.iansmathew@gmail.com>
2025-07-29 23:33:30 +0530

    feat: Add homebrew support
     - Update `llama-swap.rb` to version 144, including macOS and Linux support.
       4 - Add GitHub Actions workflow to automatically update the formula on new releases.

---
+++ Formula/llama-swap.rb
@@ -0,0 +1,35 @@
+class LlamaSwap < Formula
+  desc "Proxy server for swapping models on llama.cpp"
+  homepage "https://github.com/mostlygeek/llama-swap"
+  version "144"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/mostlygeek/llama-swap/releases/download/v#{version}/llama-swap_#{version}_darwin_arm64.tar.gz"
+      sha256 "5010a4e21c1c94ccf46e6915f373667963d42c1a62ff39f7453cf6496c6c6852"
+    end
+    on_intel do
+      url "https://github.com/mostlygeek/llama-swap/releases/download/v#{version}/llama-swap_#{version}_darwin_amd64.tar.gz"
+      sha256 "734ee66be826a81b1cb2c0b4550d865a842918bc3d459f3d20d083d390aa0809"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/mostlygeek/llama-swap/releases/download/v#{version}/llama-swap_#{version}_linux_arm64.tar.gz"
+      sha256 "10580f69b49f011d310e3d8555a58933f5d18b5467f17f3d503265a410529fda"
+    end
+    on_intel do
+      url "https://github.com/mostlygeek/llama-swap/releases/download/v#{version}/llama-swap_#{version}_linux_amd64.tar.gz"
+      sha256 "e2901e5aa2f7a2fd1f4ff7659d91d2b98a1cd470eff6cff04ae89f6b235042f7"
+    end
+  end
+
+  def install
+    bin.install "llama-swap"
+  end
+
+  test do
+    system "#{bin}/llama-swap", "--version"
+  end
+end
